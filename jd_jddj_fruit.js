d48413f70e7f0cdec9ddb5afb15dc487862e46ca
JCsolely <jcsolely@gmail.com>
2021-07-10 13:57:32 +0800

    Update jd_jddj_fruit.js

--- jd_jddj_fruit.js
+++ jd_jddj_fruit.js
@@ -4,7 +4,7 @@
 cron "25 0,9,18 * * *" script-path=jd_jddj_fruit.js, tag=京东到家果园
 */
 
-const $ = new Env('京东到家果园');
+const $title = new Env('京东到家果园');
 let isNotify = true;//是否通知,仅限nodejs
 let ckPath = './jdCookie.js';//ck路径,环境变量:JDDJ_CKPATH
 const $ = new API("jd_jddj_fruit");
