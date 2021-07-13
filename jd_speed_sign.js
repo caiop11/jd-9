2c00d5a14ca02efd061e050545df2834443f5edb
JCsolely <jcsolely@gmail.com>
2021-07-13 14:01:54 +0800

    Update jd_speed_sign.js

--- jd_speed_sign.js
+++ jd_speed_sign.js
@@ -25,8 +25,6 @@ cron "0 7 * * *" script-path=jd_speed_sign.js,tag=京东极速版
 
 const $ = new Env('京东极速版');
 
-const $ = new Env('京东极速版');
-
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
